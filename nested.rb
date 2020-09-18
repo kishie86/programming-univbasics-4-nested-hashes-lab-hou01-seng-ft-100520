3e7f73dc6bd3c42cd8858412e2936efcc6aaab99
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-18 20:03:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,7 +62,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
- # programmer_hash[:yukihiro_matsumoto] => { :known_for => "Ruby", :languages => ["LISP", "C"] }
   
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
@@ -85,8 +84,9 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
-    }
-#[:yukihiro_matsumoto] => { :known_for => "Ruby", :languages => ["LISP", "C"] }
+    } 
+    programmer_hash[:yukihiro_matsumoto] = {}
+
 
 end
 
