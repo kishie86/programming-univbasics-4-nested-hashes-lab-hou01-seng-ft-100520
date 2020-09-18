5c658b03fd98d92b5c9dbcb5374b9b095c61c705
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-18 20:43:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,8 +38,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-   programmer_hash[:alan_kay][:known_for] = "Object Orientation" 
-   programmer_hash
+    "Object Orientation" 
+   
 end
 
 def dennis_ritchies_language
